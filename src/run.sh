32ff633cfa42d0d7d168c343ab238573abb158fd
Cedric Mesnage <cedricmesnage@MD4DXGQ2P9.local>
2024-08-12 09:15:23 +0200

    LR and args

--- src/run.sh
+++ src/run.sh
@@ -16,4 +16,4 @@ source ~/.bashrc
 
 conda activate anemoi
 
-python3 ex3.py non-linear ../data/earth_train.nc ../data_earth_test.nc 8 512 5 10 "air_temperature,upward_air_velocity,geopotential_height,x_wind,y_wind"
+python3 anemoi.py non-linear ../data/earth_train.nc ../data_earth_test.nc 8 512 5 10 "air_temperature,upward_air_velocity,geopotential_height,x_wind,y_wind"
