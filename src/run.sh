2f0c1597840654d435c85e6e78508c00271cbd66
cedricidsai <c.s.mesnage@exeter.ac.uk>
2024-08-13 11:56:37 +0100

    dims

--- src/run.sh
+++ src/run.sh
@@ -16,4 +16,4 @@ source ~/.bashrc
 
 conda activate anemoi
 
-python3 anemoi.py non-linear ../data/earth_train.nc ../data_earth_test.nc 8 512 5 10 "air_temperature,upward_air_velocity,geopotential_height,x_wind,y_wind"
+python3 anemoi.py non-linear ../data/earth_train.nc ../data/earth_test.nc 8 512 5 10 "temperature,geopotential,vertical_velocity,u_component_of_wind,v_component_of_wind"
