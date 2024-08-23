cc4c6aa2ed2d33d928b067fbe3b17192f4fb8e76
cedricidsai <c.s.mesnage@exeter.ac.uk>
2024-08-23 07:38:47 +0100

    sbatch

---
+++ src/onestep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+
+#SBATCH --partition=small
+
+#SBATCH --gres=gpu:1
+
+#SBATCH --ntasks-per-node=1
+
+#SBATCH --mail-type=ALL
+
+#SBATCH --mail-user=cedric.pixel7@gmail.com
+
+source ~/.bashrc
+
+conda activate anemoi
+
+python3 anemoi.py non-linear ../data/earth_train.nc ../data/earth_test.nc 8 512 2 100 "temperature,geopotential,vertical_velocity,u_component_of_wind,v_component_of_wind"
